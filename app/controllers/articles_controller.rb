2df75d33f280c52418f716dae63808b06a15634d
mythLabs <amitbisht744@gmail.com>
2019-01-03 18:56:48 +0530

    Added delete action and form refactored to partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,54 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
   def new
-   @article = Article.new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find_by_id(params[:id])
   end
 
   def create
-    @article =Article.new(article_params)
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to article_path(@article)
     else
-      render 'new'
+      render "new"
     end
-    
-   end
+  end
 
-   def show
-     @article = Article.find_by_id(params[:id])
-   end
+  def update
+    @article = Article.find_by_id(params[:id])
 
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
 
-   private
-   def article_params
+  def show
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def destroy
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article deleted successfully"
+
+    redirect_to articles_path
+
+  end
+
+  private
+
+  def article_params
     params.require(:article).permit(:title, :description)
-   end
-
-
+  end
 end
