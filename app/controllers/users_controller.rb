a5da5ea042f3cd92f922fa64d9310bfb942a68df
mythLabs <amitbisht744@gmail.com>
2019-02-12 19:32:45 +0530

    Show user and his articles with gravitar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,10 @@ class UsersController < ApplicationController
 
     end
 
+    def show
+        @user = User.find_by_id(params[:id])
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def gravitar_for(user,options = {size:80})
+        gravitar_id = Digest::MD5::hexdigest(user&.email&.downcase)
+        size = options[:size]
+        gravital_url = "https://www.gravatar.com/avatar/#{gravitar_id}?s=#{size}"
+        image_tag(gravital_url, alt:user.username, class: "img-circle" )
+    end
+
 end
