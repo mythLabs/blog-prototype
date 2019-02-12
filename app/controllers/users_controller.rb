fe3041fc68a88aab597192c6990d3257cad2acd8
mythLabs <amitbisht744@gmail.com>
2019-02-12 19:01:19 +0530

    Added user edit and refactor user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+      @user = User.find_by_id(params[:id])
+    end
+
+    def update
+        @user = User.find_by_id(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Account was updated successfully"
+            redirect_to articles_path
+          else
+            render "edit"
+          end
+
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
