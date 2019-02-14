6069336f7eda4ff76b1d0e40872df38fec9c0b73
mythLabs <amitbisht744@gmail.com>
2019-02-14 08:47:46 +0530

    Login user on sign up ,redirect to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id]= @user.id
             flash[:success] = "Hey #{@user.username}, welcome to bloggy"
-             redirect_to articles_path
+             redirect_to user_path(@user)
         else
             render 'new'
         end
